825ba35033cc87f7810db0ad0c84066c527fd95d
zxh <1335194143@qq.com>
2023-04-11 10:56:11 +0800

    feat: init ddd project

---
+++ chatbot-api-interfaces/src/main/java/cn/bugstack/chatbot/api/ApiApplication.java
@@ -0,0 +1,16 @@
+package cn.bugstack.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxh
+ * @description 启动入口
+ */
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiApplication.class,args);
+    }
+}
---
+++ chatbot-api-interfaces/src/test/java/cn/bugstack/chatbot/api/test/ApiTest.java
@@ -0,0 +1,4 @@
+package cn.bugstack.chatbot.api.test;
+
+public class ApiTest {
+}
